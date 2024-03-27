fe349b70d301a13ca9dee91d816e7fe9ef1d83fc
0x06 <56885523+0x000006@users.noreply.github.com>
2024-03-27 19:19:28 +0100

    Privacy MOTD option

---
+++ src/main/java/griefingutils/mixin/MultiplayerServerListWidget$ServerEntryMixin.java
@@ -0,0 +1,30 @@
+package griefingutils.mixin;
+
+import griefingutils.modules.Privacy;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
+import net.minecraft.client.network.ServerInfo;
+import net.minecraft.screen.ScreenTexts;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MultiplayerServerListWidget.ServerEntry.class)
+public abstract class MultiplayerServerListWidget$ServerEntryMixin {
+    @Shadow @Final private ServerInfo server;
+
+    @Shadow protected abstract boolean pinged();
+
+    @Inject(method = "render", at = @At("HEAD"))
+    private void overwriteMOTDGet(CallbackInfo ci) {
+        Privacy privacy = Modules.get().get(Privacy.class);
+        Text original = server.label;
+        if (privacy.isActive()) {
+            server.label = privacy.transformMOTD(original, pinged() && (server.playerCountLabel != null && server.playerCountLabel != ScreenTexts.EMPTY));
+        } else server.label = original;
+    }
+}
