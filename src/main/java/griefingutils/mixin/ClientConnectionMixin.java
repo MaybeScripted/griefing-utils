fba3c6d498a6598972f6bcdd3d8911cbf316d8cd
0x06 <56885523+0x000006@users.noreply.github.com>
2024-04-18 00:09:54 +0200

    Disable cursed mixin again

--- src/main/java/griefingutils/mixin/ClientConnectionMixin.java
+++ src/main/java/griefingutils/mixin/ClientConnectionMixin.java
@@ -1,19 +1,11 @@
 package griefingutils.mixin;
 
-import griefingutils.modules.AntiCrash;
-import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.network.ClientConnection;
-import net.minecraft.network.listener.PacketListener;
-import net.minecraft.network.packet.Packet;
-import net.minecraft.network.packet.s2c.play.BundleS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ClientConnection.class, priority = Integer.MIN_VALUE)
 public class ClientConnectionMixin {
-    @Inject(method = "handlePacket", at = @At("HEAD"), cancellable = true)
+    /*@Inject(method = "handlePacket", at = @At("HEAD"), cancellable = true)
     private static <T extends PacketListener> void handlePacket(Packet<T> packet, PacketListener listener, CallbackInfo ci) {
         if (packet instanceof BundleS2CPacket p) {
             AntiCrash ac = Modules.get().get(AntiCrash.class);
@@ -24,5 +16,5 @@ public class ClientConnectionMixin {
                 ci.cancel();
             }
         }
-    }
+    }*/
 }
